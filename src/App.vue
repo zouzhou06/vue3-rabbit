56879ddd557f0aa217e2c4cee93718b89f393c13
zouzhou06 <zouzhou951@gmail.com>
2025-10-17 16:37:09 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
 <template>
    <!-- 一级路由出口组件 -->
    <RouterView/>
+   <div class="test">
+      test scss
+   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.test {
+   color:$priceColor;
+}
+
+
+</style>
