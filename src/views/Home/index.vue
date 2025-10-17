f538bbc4f919f3ae54f80f9c2782e2f204bb4caf
zouzhou06 <zouzhou951@gmail.com>
2025-10-17 21:25:04 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,6 +6,8 @@ import HomeNew from '@/views/Home/components/HomeNew.vue'
 import HomeHot from '@/views/Home/components/HomeHot.vue'
 import HomeProduct from '@/views/Home/components/HomeProduct.vue'
 
+import HomePanner from './components/HomePanner.vue'
+
 </script>
 
 <template>
@@ -16,4 +18,17 @@ import HomeProduct from '@/views/Home/components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+
+  <!-- 测试面板组件 -->
+  <HomePanner title="新鲜好物" subTitle="新鲜好物，好多商品" >
+    <div>
+      我是新鲜好物的插槽
+    </div>
+  </HomePanner>
+    <div>
+      我是人气推荐的插槽
+    </div>
+  <HomePanner title="人气推荐" subTitle="人气推荐，好多商品">
+
+  </HomePanner>
 </template>
