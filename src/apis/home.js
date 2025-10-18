102baf3fb87116cef51133cd003b9f2beb89ce7c
zouzhou06 <zouzhou951@gmail.com>
2025-10-18 11:25:00 +0800

    Product产品列表实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,12 +1,18 @@
-import  httpInstance  from '@/utils/http'
+import httpInstance from '@/utils/http'
+
 
 // 获取banner
-export function getBannerAPI() {
-    return httpInstance({
-        url:'/home/banner'
-    })
-}
 
+export function getBannerAPI (params = {}) {
+  // 默认为1 商品为2
+  const { distributionSite = '1' } = params
+  return httpInstance({
+    url: '/home/banner',
+    params: {
+      distributionSite
+    }
+  })
+}
 
 /**
  * @description: 获取新鲜好物
@@ -15,7 +21,7 @@ export function getBannerAPI() {
  */
 export const findNewAPI = () => {
   return httpInstance({
-    url:'/home/new'
+    url: '/home/new'
   })
 }
 
@@ -27,5 +33,16 @@ export const findNewAPI = () => {
 export const getHotAPI = () => {
   return httpInstance({
     url: '/home/hot'
+  })
+}
+
+/**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: '/home/goods'
   })
 }
