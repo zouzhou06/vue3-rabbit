391633f05e354cd34797af0ddac7243965adcf6a
zouzhou06 <zouzhou951@gmail.com>
2025-10-18 10:40:48 +0800

    懒加载及优化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
 import App from './App.vue'
 import router from './router'
 
@@ -9,12 +10,24 @@ const app = createApp(App)
 //引入初始化样式
 import '@/styles/common.scss'
 
-
+// 引入插件
+import {lazyPlugin} from '@/directives/index'
 
 
 
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
+app.use(lazyPlugin)
+// 定义全局指令
+// 定义懒加载插件
+
+
+ 
+   
+
+ 
+
+
+
